c99a1ecd5a5a823ee275473bb7345676d97578d5
maslov <maslov@67c690ed-b279-284f-999c-0e6cdc159a9d>
2008-02-20 12:01:31 +0000

    Added Makefile for jars building
    git-svn-id: svn://10.81.1.1/svnroot/fb/trunk/rsfb_head@2322 67c690ed-b279-284f-999c-0e6cdc159a9d

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# Checking JAVA_HOME and ANT_HOME environment variables
+if test "$JAVA_HOME" = ""; then
+    echo "Error: JAVA_HOME environment variable must be set"
+    exit 1
+fi
+	
+if test "$ANT_HOME" = ""; then
+    echo "Error: ANT_HOME environment variable must be set"
+    exit 1
+fi
+
 CLASSPATH=
 #:${JAVA_HOME}/lib/tools.jar
 
@@ -7,7 +18,6 @@ TARGET_CLASSPATH=`echo ../../lib/*.jar | tr ' ' ':'`
 
 TARGET_CLASSPATH=${TARGET_CLASSPATH}:${JAVA_HOME}/lib/tools.jar
 
-ANT_HOME=.
 ANT=$ANT_HOME/bin/ant
 #xerces/xalan for test support.
 JAXP_DOM_FACTORY="org.apache.xerces.jaxp.DocumentBuilderFactoryImpl"
@@ -16,7 +26,4 @@ JAXP_SAX_FACTORY="org.apache.xerces.jaxp.SAXParserFactoryImpl"
 ANT_OPTS="$ANT_OPTS -Djavax.xml.parsers.DocumentBuilderFactory=$JAXP_DOM_FACTORY"
 ANT_OPTS="$ANT_OPTS -Djavax.xml.parsers.SAXParserFactory=$JAXP_SAX_FACTORY"
 
-    export ANT ANT_HOME ANT_OPTS
-
-
-    exec $ANT $ANT_OPTIONS "$@"
+exec $ANT $ANT_OPTIONS "$@"
