115d347465933083644fddbd3c58dc1426c414bb
Artyom Smirnov <artyom.smirnov@red-soft.biz>
2016-02-16 12:12:17 +0300

    Fix notest build

--- build.sh
+++ build.sh
@@ -59,9 +59,4 @@ TARGET_CLASSPATH=`echo ../../lib/*.jar | tr ' ' ':'`
 
 TARGET_CLASSPATH=${TARGET_CLASSPATH}:${JAVA_HOME}/lib/tools.jar
 
-ANT_HOME=.
-ANT=$ANT_HOME/bin/ant
-
-export ANT ANT_HOME ANT_OPTS
-
-exec $ANT $ANT_OPTIONS "$@"
+exec $ANT $ANT_OPTS
