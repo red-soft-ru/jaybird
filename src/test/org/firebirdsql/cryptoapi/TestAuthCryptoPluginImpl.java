b0708cd4744a614b2979978551f20eb43d315f68
Vasiliy Yashkov <vasiliy.yashkov@red-soft.biz>
2017-10-11 12:16:56 +0300

    [test] Test for creating and destroying hash using crypto plugin

--- src/test/org/firebirdsql/cryptoapi/TestAuthCryptoPluginImpl.java
+++ src/test/org/firebirdsql/cryptoapi/TestAuthCryptoPluginImpl.java
@@ -6,6 +6,10 @@ import org.firebirdsql.gds.impl.wire.auth.AuthCryptoPlugin;
 import org.firebirdsql.cryptoapi.AuthCryptoPluginImpl;
 
 public class TestAuthCryptoPluginImpl extends TestCase {
+
+    private byte[] testbuf = "xxThis is a test of the crypto plugin".getBytes();
+    private Object hash = null;
+
     public TestAuthCryptoPluginImpl(String name) {
         super(name);
     }
@@ -19,4 +23,17 @@ public class TestAuthCryptoPluginImpl extends TestCase {
         AuthCryptoPlugin.register(new AuthCryptoPluginImpl());
     }
 
+    public void testAuthCryptoPluginImpl_createHash() throws Exception {
+        AuthCryptoPlugin plugin = AuthCryptoPlugin.getPlugin();
+        hash = plugin.createHash(testbuf);
+        assertNotNull(hash);
+    }
+
+    public void testAuthCryptoPluginImpl_destroyHash() throws Exception {
+        if (hash == null)
+            testAuthCryptoPluginImpl_createHash();
+        AuthCryptoPlugin plugin = AuthCryptoPlugin.getPlugin();
+        boolean res = plugin.destroyHash(hash);
+        assertTrue(res);
+    }
 }
