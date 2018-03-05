0dfd88f2272c6cfc0cf839d3a0eaaa51c184eaf3
Vasiliy Yashkov <vasiliy.yashkov@red-soft.biz>
2018-03-05 09:52:42 +0300

    Added the server certificate verification test
    Ignored until the error is resolved with the server certificate open #25043

--- src/test/org/firebirdsql/gds/impl/wire/auth/TestAuthSspi.java
+++ src/test/org/firebirdsql/gds/impl/wire/auth/TestAuthSspi.java
@@ -7,6 +7,7 @@ import org.firebirdsql.common.JdbcResourceHelper;
 import org.firebirdsql.cryptoapi.AuthCryptoPluginImpl;
 import org.firebirdsql.gds.impl.GDSType;
 import org.firebirdsql.jca.FBSADataSource;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.sql.Connection;
@@ -111,6 +112,45 @@ public class TestAuthSspi extends FBJUnit4TestBase {
         fbDataSource.setNonStandardProperty("isc_dpb_repository_pin", "12345678");
         fbDataSource.setNonStandardProperty("isc_dpb_trusted_auth", "1");
         fbDataSource.setNonStandardProperty("isc_dpb_multi_factor_auth", "1");
+
+        Connection conn = null;
+        try {
+            conn = fbDataSource.getConnection();
+
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("select current_user from rdb$database");
+            resultSet.next();
+            System.out.println("Current user is " + resultSet.getString(1));
+            JdbcResourceHelper.closeQuietly(resultSet);
+            JdbcResourceHelper.closeQuietly(statement);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Statement should not fail");
+        } finally {
+            JdbcResourceHelper.closeQuietly(conn);
+            fbDataSource.close();
+        }
+    }
+
+    @Test
+    @Ignore
+    // TODO enable test after fix error with open server certificate #25043
+    public void testVerifyServerCertificate() throws Exception {
+        initLogger();
+
+        AuthCryptoPlugin.register(new AuthCryptoPluginImpl());
+
+        final FBSADataSource fbDataSource = new FBSADataSource(GDSType.getType("PURE_JAVA"));
+
+        fbDataSource.setDatabase(FBTestProperties.DB_DATASOURCE_URL);
+        fbDataSource.setNonStandardProperty("isc_dpb_lc_ctype", "WIN1251");
+        fbDataSource.setNonStandardProperty("isc_dpb_user_name", "artyom.smirnov@red-soft.ru");
+        fbDataSource.setNonStandardProperty("isc_dpb_password", "q3rgu7Ah");
+        fbDataSource.setNonStandardProperty("isc_dpb_certificate", "testuser.cer");
+        fbDataSource.setNonStandardProperty("isc_dpb_repository_pin", "12345678");
+        fbDataSource.setNonStandardProperty("isc_dpb_trusted_auth", "1");
+        fbDataSource.setNonStandardProperty("isc_dpb_multi_factor_auth", "1");
+        fbDataSource.setNonStandardProperty("isc_dpb_verify_server", "1");
 
         Connection conn = null;
         try {
