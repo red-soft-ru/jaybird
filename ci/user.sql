73b9a5ed338d60041f15522a1c0b9f16fa83fc37
Vasiliy Yashkov <vasiliy.yashkov@red-soft.biz>
2018-03-14 12:55:19 +0300

    [ci] Using Red Database 3 for tests

---
+++ ci/user.sql
@@ -0,0 +1,7 @@
+CREATE GLOBAL MAPPING MULTIFACTOR_AUTH USING PLUGIN MULTIFACTOR FROM ANY USER TO USER;
+commit;
+create user "SYSDBA" password 'masterkey';
+commit;
+create user "ARTYOM.SMIRNOV@RED-SOFT.RU" password 'q3rgu7Ah';
+commit;
+
