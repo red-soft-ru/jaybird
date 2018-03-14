bab37dfdfbcb01376333f2ef51675c8298d1aded
Vasiliy Yashkov <vasiliy.yashkov@red-soft.biz>
2018-03-14 18:06:29 +0300

    [ci] User SYSDBA already created

--- ci/user.sql
+++ ci/user.sql
@@ -1,6 +1,4 @@
 CREATE GLOBAL MAPPING MULTIFACTOR_AUTH USING PLUGIN MULTIFACTOR FROM ANY USER TO USER;
 commit;
-create user "SYSDBA" password 'masterkey';
-commit;
 create user "ARTYOM.SMIRNOV@RED-SOFT.RU" password 'q3rgu7Ah' using plugin Multifactor_Manager;
 commit;
