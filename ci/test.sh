08a44cd717fda47f00e3df2da418de8258a98f1a
Vasiliy Yashkov <vasiliy.yashkov@red-soft.biz>
2018-08-31 10:57:54 +0300

    [ci] Use Red Database 4 for tests

--- ci/test.sh
+++ ci/test.sh
@@ -54,7 +54,7 @@ check_variable JAVA_HOME
 check_variable WORKSPACE
 
 OS=linux
-RDB_VERSION=3.0.0.917
+RDB_VERSION=4.0.0.978
 TEST_DIR=/tmp/jaybird_test
 ARCH=`arch`
 if [ "$ARCH" == "i686" ]; then
