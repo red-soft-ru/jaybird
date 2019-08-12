13669b71af665b2c4d18dce42a7c15d28cfb265f
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2019-08-12 16:46:19 +0300

    When testing a trusted certificate, use a different username
    To avoid a match with the certificate username.

--- src/test/org/firebirdsql/gds/impl/wire/auth/TestAuthSspi.java
+++ src/test/org/firebirdsql/gds/impl/wire/auth/TestAuthSspi.java
@@ -183,7 +183,7 @@ public class TestAuthSspi extends FBJUnit4TestBase {
 
         fbDataSource.setDatabase(FBTestProperties.DB_DATASOURCE_URL);
         fbDataSource.setNonStandardProperty("isc_dpb_lc_ctype", "WIN1251");
-        fbDataSource.setNonStandardProperty("isc_dpb_user_name", "artyom.smirnov@red-soft.ru");
+        fbDataSource.setNonStandardProperty("isc_dpb_user_name", "trusted_user");
         fbDataSource.setNonStandardProperty("isc_dpb_certificate", "testuser.cer");
         fbDataSource.setNonStandardProperty("isc_dpb_repository_pin", "12345678");
         fbDataSource.setNonStandardProperty("isc_dpb_trusted_auth", "1");
@@ -197,7 +197,7 @@ public class TestAuthSspi extends FBJUnit4TestBase {
             ResultSet resultSet = statement.executeQuery("select current_user from rdb$database");
             resultSet.next();
             System.out.println("Current user is " + resultSet.getString(1));
-            assertEquals("artyom.smirnov@red-soft.ru", resultSet.getString(1).toLowerCase());
+            assertEquals("trusted_user", resultSet.getString(1).toLowerCase());
             JdbcResourceHelper.closeQuietly(resultSet);
             JdbcResourceHelper.closeQuietly(statement);
         } catch (Exception e) {
