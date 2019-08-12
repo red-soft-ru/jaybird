ced6fb4bb1d7910d969e5c217cc76aa90c666417
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2019-08-12 16:48:30 +0300

    When testing a trusted certificate, use a different username
    To avoid a match with the certificate username.

--- ci/user3.sql
+++ ci/user3.sql
@@ -4,3 +4,5 @@ CREATE GLOBAL MAPPING GSS_AUTH USING PLUGIN GSS FROM ANY USER TO USER;
 commit;
 create user "ARTYOM.SMIRNOV@RED-SOFT.RU" password 'q3rgu7Ah' using plugin Multifactor_Manager;
 commit;
+create user trusted_user password 'trusted' using plugin Multifactor_Manager;
+commit;
