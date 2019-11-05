197e90f5dfad3e2d935436d5f7edbb6781ca72c0
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2019-11-05 14:22:18 +0300

    See #43640. Documentation for full jars

--- ci/build.sh
+++ ci/build.sh
@@ -33,6 +33,7 @@ mkdir -p ${CI_PROJECT_DIR}/dist/jdk18/bin \
 cp modules/jaybird-jdk18/target/jaybird-*javadoc* ${CI_PROJECT_DIR}/dist/jdk18/javadoc
 cp modules/jaybird-jdk18/target/jaybird* ${CI_PROJECT_DIR}/dist/jdk18/bin
 cp modules/jaybird-jdk18/target/jaybird-*sources* ${CI_PROJECT_DIR}/dist/jdk18/sources
+cp modules/jaybird-full-jdk18/target/jaybird-*javadoc* ${CI_PROJECT_DIR}/dist/jdk18/javadoc
 cp modules/jaybird-full-jdk18/target/jaybird* ${CI_PROJECT_DIR}/dist/jdk18/bin
 cp modules/jaybird-full-jdk18/target/jaybird-*sources* ${CI_PROJECT_DIR}/dist/jdk18/sources
 cp modules/cryptoapi-security-jdk18/target/jaybird* ${CI_PROJECT_DIR}/dist/jdk18/bin
@@ -43,6 +44,7 @@ cp modules/cryptoapi-jdk18/target/jaybird-*sources* ${CI_PROJECT_DIR}/dist/jdk18
 cp modules/jaybird-jdk17/target/jaybird-*javadoc* ${CI_PROJECT_DIR}/dist/jdk17/javadoc
 cp modules/jaybird-jdk17/target/jaybird* ${CI_PROJECT_DIR}/dist/jdk17/bin
 cp modules/jaybird-jdk17/target/jaybird-*sources* ${CI_PROJECT_DIR}/dist/jdk17/sources
+cp modules/jaybird-full-jdk17/target/jaybird-*javadoc* ${CI_PROJECT_DIR}/dist/jdk17/javadoc
 cp modules/jaybird-full-jdk17/target/jaybird* ${CI_PROJECT_DIR}/dist/jdk17/bin
 cp modules/jaybird-full-jdk17/target/jaybird-*sources* ${CI_PROJECT_DIR}/dist/jdk17/sources
 cp modules/cryptoapi-security-jdk17/target/jaybird* ${CI_PROJECT_DIR}/dist/jdk17/bin
