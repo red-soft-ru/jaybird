1dfb5a7ef2b4ac28956b152bf2799555865da4c3
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2019-11-19 11:16:45 +0300

    Set java virtual machine paths before testing

--- ci/test.sh
+++ ci/test.sh
@@ -221,6 +221,6 @@ done
 echo rdb_server | kinit rdb_server/localhost
 klist
 
-mvn $MAVEN_CLI_OPTS -f "${CI_PROJECT_DIR}"/pom.xml test -Pdeploy-internal -DreportNamePrefix=$REPORT_PREFIX -DreleaseHubBuildVersion=$VERSION  -DfailIfNoTests=false -Dtest.db.dir=$TEST_DIR -Dtest.java7.skip=$SKIP_JAVA7_TEST -Dtest.java8.jvm=$JAVA_HOME/bin/java -Dtest.java7.jvm=/usr/lib/jvm/jre-1.7.0/bin/java -Dtest=$TEST_LIST
+mvn $MAVEN_CLI_OPTS -f "${CI_PROJECT_DIR}"/pom.xml test -Pdeploy-internal -DreportNamePrefix=$REPORT_PREFIX -DreleaseHubBuildVersion=$VERSION  -DfailIfNoTests=false -Dtest.db.dir=$TEST_DIR -Dtest.java7.skip=$SKIP_JAVA7_TEST -Dtest.java8.jvm=$TEST_JAVA8_JVM -Dtest.java7.jvm=$TEST_JAVA7_JVM -Dtest=$TEST_LIST
 
 kdestroy
