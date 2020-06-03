9ff0aff51d8a2a2223154d60382608eb475bfe05
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2020-06-03 17:53:13 +0300

    Fixed comparison of integer values

--- ci/test.sh
+++ ci/test.sh
@@ -199,7 +199,7 @@ done
 echo rdb_server | kinit rdb_server/localhost
 klist
 
-if [[ "$RDB_MAJOR_VERSION" >= "3" ]]; then
+if [[ "$RDB_MAJOR_VERSION" -ge "3" ]]; then
   mvn $MAVEN_CLI_OPTS -f "${CI_PROJECT_DIR}"/pom.xml test -Pdeploy-internal -DreportNamePrefix=$REPORT_PREFIX -DreleaseHubBuildVersion=$VERSION  -DfailIfNoTests=false -Dtest.db.dir=$TEST_DIR -Dtest.jni.skip=false -Dtest.java.jvm=$TEST_JAVA_JVM -Dtest=$TEST_LIST
 else
   mvn $MAVEN_CLI_OPTS -f "${CI_PROJECT_DIR}"/pom.xml test -Pdeploy-internal -DreportNamePrefix=$REPORT_PREFIX -DreleaseHubBuildVersion=$VERSION  -DfailIfNoTests=false -Dtest.db.dir=$TEST_DIR -Dtest.java.jvm=$TEST_JAVA_JVM -Dtest=$TEST_LIST
