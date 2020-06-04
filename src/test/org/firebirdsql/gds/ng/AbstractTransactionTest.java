8ea92114cb398ea4ed3d41dcb1e7b8d2f8db6541
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2020-06-04 11:48:39 +0300

    See #50227. Test for zero lock timeout

--- src/test/org/firebirdsql/gds/ng/AbstractTransactionTest.java
+++ src/test/org/firebirdsql/gds/ng/AbstractTransactionTest.java
@@ -170,6 +170,21 @@ public abstract class AbstractTransactionTest extends FBJUnit4TestBase {
         assertValueForKey(key, false, null);
     }
 
+    @Test
+    public void testTransactionWithZeroLockTimeout() throws Exception {
+        expectedException.expect(SQLException.class);
+
+        TransactionParameterBuffer tpb = new TransactionParameterBufferImpl();
+        tpb.addArgument(ISCConstants.isc_tpb_read_committed);
+        tpb.addArgument(ISCConstants.isc_tpb_rec_version);
+        tpb.addArgument(ISCConstants.isc_tpb_write);
+        tpb.addArgument(ISCConstants.isc_tpb_wait);
+        tpb.addArgument(ISCConstants.isc_tpb_lock_timeout, 0);
+        final FbTransaction transaction = db.startTransaction(tpb);
+
+        expectedException.expect(SQLException.class);
+    }
+
     protected final void assertValueForKey(int key, boolean hasRow, String expectedValue) throws SQLException {
         Connection connection = getConnectionViaDriverManager();
         PreparedStatement pstmt = null;
