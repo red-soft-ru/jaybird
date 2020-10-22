e6f972f1d8326540d248efcffcab9a3533bc4b60
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2020-10-22 15:43:31 +0300

    See #54722. Use jaybird logging instead of log4j library

--- src/test/org/firebirdsql/gds/impl/wire/auth/TestAuthSspi.java
+++ src/test/org/firebirdsql/gds/impl/wire/auth/TestAuthSspi.java
@@ -1,8 +1,5 @@
 package org.firebirdsql.gds.impl.wire.auth;
 
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.core.config.Configurator;
-import org.apache.logging.log4j.core.config.DefaultConfiguration;
 import org.firebirdsql.common.FBJUnit4TestBase;
 import org.firebirdsql.common.FBTestProperties;
 import org.firebirdsql.common.JdbcResourceHelper;
@@ -30,14 +27,8 @@ import static org.junit.Assert.fail;
  */
 public class TestAuthSspi extends FBJUnit4TestBase {
 
-    public static void initLogger() {
-        Configurator.initialize(new DefaultConfiguration());
-        Configurator.setRootLevel(Level.INFO);
-    }
-
     @Test
     public void testMultifactorAuthCertificateOnly() throws Exception {
-        initLogger();
 
         try (Connection connection = getConnectionViaDriverManager();
             Statement statement = connection.createStatement()) {
@@ -86,7 +77,6 @@ public class TestAuthSspi extends FBJUnit4TestBase {
 
     @Test
     public void testMultifactorAuthPasswordOnly() throws Exception {
-        initLogger();
 
         try (AuthCryptoPluginImpl acp = new AuthCryptoPluginImpl()) {
             AuthCryptoPlugin.register(acp);
@@ -126,7 +116,6 @@ public class TestAuthSspi extends FBJUnit4TestBase {
 
     @Test
     public void testMultifactorAuthPasswordAndCertificate() throws Exception {
-        initLogger();
 
         try (Connection connection = getConnectionViaDriverManager();
              Statement statement = connection.createStatement()) {
@@ -176,7 +165,6 @@ public class TestAuthSspi extends FBJUnit4TestBase {
 
     @Test
     public void testTrustedCertificate() throws Exception {
-        initLogger();
 
         try (AuthCryptoPluginImpl acp = new AuthCryptoPluginImpl()) {
             AuthCryptoPlugin.register(acp);
@@ -214,7 +202,6 @@ public class TestAuthSspi extends FBJUnit4TestBase {
 
     @Test
     public void testVerifyServerCertificate() throws Exception {
-        initLogger();
 
         try (AuthCryptoPluginImpl acp = new AuthCryptoPluginImpl()) {
             AuthCryptoPlugin.register(acp);
@@ -254,7 +241,6 @@ public class TestAuthSspi extends FBJUnit4TestBase {
 
     @Test
     public void testTrustedUser() throws Exception {
-        initLogger();
 
         try (AuthCryptoPluginImpl acp = new AuthCryptoPluginImpl()) {
             AuthCryptoPlugin.register(acp);
