606e66304d1cb5c01af83a1466794ea9e5a92c2d
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2020-11-30 13:33:58 +0300

    Add additional checks to the test for logical connections. See #53866
    Refactoring has also been done.

--- src/test/org/firebirdsql/jca/TestFBSADataSource.java
+++ src/test/org/firebirdsql/jca/TestFBSADataSource.java
@@ -2,54 +2,70 @@ package org.firebirdsql.jca;
 
 import org.junit.Test;
 
-import static org.firebirdsql.common.FBTestProperties.DB_DATASOURCE_URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.firebirdsql.common.FBTestProperties.*;
 
 public class TestFBSADataSource extends TestXABase {
 
     @Test
     public void testFBSADataSource() throws Exception {
 
-        org.firebirdsql.jca.FBSADataSource dataSource =
-                new org.firebirdsql.jca.FBSADataSource();
+        final FBSADataSource dataSource = new FBSADataSource();
 
         // Set the standard properties
-        dataSource.setDatabase (DB_DATASOURCE_URL);
-        dataSource.setDescription ("An example database of employees");
-        dataSource.setUserName("sysdba");
-        dataSource.setPassword("masterkey");
-        dataSource.setEncoding("WIN1251");
+        dataSource.setDatabase(DB_DATASOURCE_URL);
+        dataSource.setDescription("An example database of employees");
+        dataSource.setUserName(DB_USER);
+        dataSource.setPassword(DB_PASSWORD);
+        dataSource.setEncoding(DB_LC_CTYPE);
         try {
-            dataSource.setLoginTimeout (10);
-            java.sql.Connection c1 = dataSource.getConnection ();
-            java.sql.Connection c2 = dataSource.getConnection ();
+            dataSource.setLoginTimeout(10);
+            final Connection c1 = dataSource.getConnection();
+            final Connection c2 = dataSource.getConnection();
             c1.setAutoCommit(false);
             c2.setAutoCommit(false);
-            java.sql.Statement stmt = c1.createStatement();
-            java.sql.Statement stmt2 = c2.createStatement();
-            java.sql.ResultSet rs2 = stmt2.executeQuery("SELECT MON$ATTACHMENT_ID,MON$SERVER_PID FROM MON$ATTACHMENTS");
-            java.sql.ResultSet rs = stmt.executeQuery("SELECT MON$TRANSACTION_ID,MON$ATTACHMENT_ID FROM MON$TRANSACTIONS");
-            while(rs.next())
+            final Statement stmt = c1.createStatement();
+            final Statement stmt2 = c2.createStatement();
+            final ResultSet rs2 = stmt2.executeQuery("SELECT MON$ATTACHMENT_ID,MON$SERVER_PID FROM MON$ATTACHMENTS");
+            final ResultSet rs = stmt.executeQuery("SELECT MON$TRANSACTION_ID,MON$ATTACHMENT_ID FROM MON$TRANSACTIONS");
+            final List<Integer> attIDs = new ArrayList<>();
+            while (rs.next()) {
+                attIDs.add(rs.getInt(2));
                 System.out.println("MON$TRANSACTION_ID = " + rs.getString(1) + ", MON$ATTACHMENT_ID = " + rs.getString(2));
+            }
+            rs.close();
+            assert attIDs.get(0).equals(attIDs.get(1));
 
-            while(rs2.next())
+            while (rs2.next()) {
+                attIDs.add(rs2.getInt(1));
                 System.out.println("MON$ATTACHMENT_ID = " + rs2.getString(1) + ", MON$SERVER_PID = " + rs2.getString(2));
+            }
+            rs2.close();
+            assert attIDs.get(0).equals(attIDs.get(2));
+
             stmt.close();
+            stmt2.close();
 
             // At this point, there is no implicit driver instance
             // registered with the driver manager!
-            System.out.println ("got connection");
-            c1.close ();
-            c2.close ();
-            java.sql.Connection c3 = dataSource.getConnection ();
-            java.sql.PreparedStatement stmt3 = c3.prepareStatement("SELECT MON$ATTACHMENT_ID,MON$SERVER_PID FROM MON$ATTACHMENTS");
-            rs = stmt3.executeQuery();
-            while (rs.next()){
-                System.out.println(rs.getInt(1));
+            System.out.println("got connection");
+            c1.close();
+            c2.close();
+            final Connection c3 = dataSource.getConnection();
+            final PreparedStatement stmt3 = c3.prepareStatement("SELECT MON$ATTACHMENT_ID,MON$SERVER_PID FROM MON$ATTACHMENTS");
+            final ResultSet rs3 = stmt3.executeQuery();
+            while (rs3.next()) {
+                System.out.println(rs3.getInt(1));
             }
-            rs.close();
+            rs3.close();
             c3.close();
-        }
-        finally {
+        } finally {
             dataSource.close();
         }
     }
