9cbd6eaaeff6b0cca3531ed8766889175540fccf
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2021-01-25 11:32:33 +0300

    Add compatibility for ng layer connection properties. See #48870

---
+++ src/main/org/firebirdsql/gds/ng/IConnectionProperties.java
@@ -0,0 +1,33 @@
+/*
+ * Firebird Open Source JavaEE Connector - JDBC Driver
+ *
+ * Distributable under LGPL license.
+ * You may obtain a copy of the License at http://www.gnu.org/copyleft/lgpl.html
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * LGPL License for more details.
+ *
+ * This file was created by members of the firebird development team.
+ * All individual contributions remain the Copyright (C) of those
+ * individuals.  Contributors to this file are either listed here or
+ * can be obtained from a source control history command.
+ *
+ * All rights reserved.
+ */
+package org.firebirdsql.gds.ng;
+
+import org.firebirdsql.jdbc.AbstractFBConnectionProperties;
+import org.firebirdsql.jdbc.FBConnectionProperties;
+import org.firebirdsql.jdbc.FirebirdConnectionProperties;
+
+/**
+ * Class for backward compatibility with connection properties
+ *
+ * @author <a href="mailto:mrotteveel@users.sourceforge.net">Mark Rotteveel</a>
+ * @author <a href="mailto:vasiliy.yashkov@red-soft.ru">Vasiliy Yashkov</a>
+ * @since 3.0
+ */
+public interface IConnectionProperties extends FirebirdConnectionProperties {
+}
