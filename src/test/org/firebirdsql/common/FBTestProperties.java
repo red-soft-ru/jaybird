58262bbda2f25989a09fa711b060c370cea7cfad
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2021-02-15 16:29:29 +0300

    Set server URL and port for OO API tests

--- src/test/org/firebirdsql/common/FBTestProperties.java
+++ src/test/org/firebirdsql/common/FBTestProperties.java
@@ -146,7 +146,8 @@ public final class FBTestProperties {
                 final FBServiceManager fbServiceManager = new FBServiceManager(gdsType);
                 if (gdsType == GDSType.getType("PURE_JAVA")
                         || gdsType == GDSType.getType("NATIVE")
-                        || gdsType == GDSType.getType("OOREMOTE") ) {
+                        || gdsType == GDSType.getType("OOREMOTE")
+                        || gdsType == GDSType.getType("FBOONATIVE") ) {
                     fbServiceManager.setServer(DB_SERVER_URL);
                     fbServiceManager.setPort(DB_SERVER_PORT);
                 }
@@ -237,7 +238,8 @@ public final class FBTestProperties {
         final GDSType gdsType = getGdsType();
         if (gdsType == GDSType.getType("PURE_JAVA")
                 || gdsType == GDSType.getType("NATIVE")
-                || gdsType == GDSType.getType("OOREMOTE")) {
+                || gdsType == GDSType.getType("OOREMOTE")
+                || gdsType == GDSType.getType("FBOONATIVE")) {
             fbManager.setServer(DB_SERVER_URL);
             fbManager.setPort(DB_SERVER_PORT);
         }
