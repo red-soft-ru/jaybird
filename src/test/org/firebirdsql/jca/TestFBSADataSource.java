578cd8b1774230fc2d142d48d0e44b742f280682
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2021-04-21 16:31:29 +0300

    Cosmetic

--- src/test/org/firebirdsql/jca/TestFBSADataSource.java
+++ src/test/org/firebirdsql/jca/TestFBSADataSource.java
@@ -1,12 +1,7 @@
 package org.firebirdsql.jca;
 
-import org.firebirdsql.cryptoapi.AuthCryptoPluginImpl;
-import org.firebirdsql.cryptoapi.cryptopro.exception.CryptoException;
 import org.firebirdsql.gds.TransactionParameterBuffer;
-import org.firebirdsql.gds.impl.wire.auth.AuthCryptoPlugin;
 import org.firebirdsql.jdbc.FBConnection;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.Connection;
@@ -44,18 +39,24 @@ public class TestFBSADataSource extends TestXABase {
             final ResultSet rs2 = stmt2.executeQuery("SELECT MON$ATTACHMENT_ID,MON$SERVER_PID FROM MON$ATTACHMENTS");
             final ResultSet rs = stmt.executeQuery("SELECT MON$TRANSACTION_ID,MON$ATTACHMENT_ID FROM MON$TRANSACTIONS");
             final List<Integer> attIDs = new ArrayList<>();
+
             while (rs.next()) {
                 attIDs.add(rs.getInt(2));
-                System.out.println("MON$TRANSACTION_ID = " + rs.getString(1) + ", MON$ATTACHMENT_ID = " + rs.getString(2));
+                System.out.println(String.format("MON$TRANSACTION_ID = %s, MON$ATTACHMENT_ID = %s",
+                        rs.getString(1), rs.getString(2)));
             }
+
             rs.close();
+
             assert attIDs.get(0).equals(attIDs.get(1));
 
             while (rs2.next()) {
                 attIDs.add(rs2.getInt(1));
                 System.out.println("MON$ATTACHMENT_ID = " + rs2.getString(1) + ", MON$SERVER_PID = " + rs2.getString(2));
             }
+
             rs2.close();
+
             assert attIDs.get(0).equals(attIDs.get(2));
 
             stmt.close();
@@ -67,7 +68,8 @@ public class TestFBSADataSource extends TestXABase {
             c1.close();
             c2.close();
             final Connection c3 = dataSource.getConnection();
-            final PreparedStatement stmt3 = c3.prepareStatement("SELECT MON$ATTACHMENT_ID,MON$SERVER_PID FROM MON$ATTACHMENTS");
+            final PreparedStatement stmt3 =
+                    c3.prepareStatement("SELECT MON$ATTACHMENT_ID,MON$SERVER_PID FROM MON$ATTACHMENTS");
             final ResultSet rs3 = stmt3.executeQuery();
             while (rs3.next()) {
                 System.out.println(rs3.getInt(1));
@@ -101,18 +103,17 @@ public class TestFBSADataSource extends TestXABase {
         connection.commit();
         connection.close();
 
-        org.firebirdsql.jca.FBSADataSource dataSource =
-                new org.firebirdsql.jca.FBSADataSource();
+        FBSADataSource dataSource = new FBSADataSource();
 
         // Set the standard properties
-        dataSource.setDatabase (DB_DATASOURCE_URL);
-        dataSource.setDescription ("An example database of employees");
+        dataSource.setDatabase(DB_DATASOURCE_URL);
+        dataSource.setDescription("An example database of employees");
         dataSource.setUserName("sysdba");
         dataSource.setPassword("masterkey");
         dataSource.setEncoding("WIN1251");
         try {
-            dataSource.setLoginTimeout (10);
-            FBConnection c1 = (FBConnection) dataSource.getConnection ();
+            dataSource.setLoginTimeout(10);
+            FBConnection c1 = (FBConnection) dataSource.getConnection();
 
 
             TransactionParameterBuffer tpb = c1.createTransactionParameterBuffer();
@@ -123,49 +124,49 @@ public class TestFBSADataSource extends TestXABase {
 
             c1.setTransactionParameters(tpb);
 
-            FBConnection c2 = (FBConnection) dataSource.getConnection ();
+            FBConnection c2 = (FBConnection) dataSource.getConnection();
 
             c1.setAutoCommit(false);
             c2.setAutoCommit(false);
-            java.sql.Statement stmt = c1.createStatement();
-            java.sql.Statement stmt2 = c2.createStatement();
-            java.sql.ResultSet rs = stmt.executeQuery("SELECT MON$TRANSACTION_ID,MON$ATTACHMENT_ID FROM MON$TRANSACTIONS");
+            Statement stmt = c1.createStatement();
+            Statement stmt2 = c2.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT MON$TRANSACTION_ID,MON$ATTACHMENT_ID FROM MON$TRANSACTIONS");
             int update = stmt2.executeUpdate("UPDATE test SET col1 = 2");
 
             assertEquals(1, update);
 
-            while(rs.next())
-                System.out.println("MON$TRANSACTION_ID = " + rs.getString(1) + ", MON$ATTACHMENT_ID = " + rs.getString(2));
+            while (rs.next()) {
+                System.out.println(String.format("MON$TRANSACTION_ID = %s, MON$ATTACHMENT_ID = %s",
+                        rs.getString(1), rs.getString(2)));
+            }
 
             stmt.close();
             stmt2.close();
 
             // At this point, there is no implicit driver instance
             // registered with the driver manager!
-            System.out.println ("got connection");
-            c1.close ();
-            c2.close ();
-            java.sql.Connection c3 = dataSource.getConnection ();
-            java.sql.PreparedStatement stmt3 = c3.prepareStatement("SELECT MON$ATTACHMENT_ID,MON$SERVER_PID FROM MON$ATTACHMENTS");
+            System.out.println("got connection");
+            c1.close();
+            c2.close();
+            Connection c3 = dataSource.getConnection();
+            PreparedStatement stmt3 = c3.prepareStatement("SELECT MON$ATTACHMENT_ID,MON$SERVER_PID FROM MON$ATTACHMENTS");
             rs = stmt3.executeQuery();
-            while (rs.next()){
+            while (rs.next()) {
                 System.out.println(rs.getInt(1));
             }
             rs.close();
             c3.close();
-        }
-        finally {
+        } finally {
             dataSource.close();
         }
     }
 
     @Test
     public void testEncryptedPassword() throws Exception {
-
-        FBSADataSource dataSource =	new FBSADataSource();
+        FBSADataSource dataSource = new FBSADataSource();
         // Set the standard properties
-        dataSource.setDatabase (DB_DATASOURCE_URL);
-        dataSource.setDescription ("An example database of employees");
+        dataSource.setDatabase(DB_DATASOURCE_URL);
+        dataSource.setDescription("An example database of employees");
         dataSource.setUserName("sysdba");
         dataSource.setEncoding("WIN1251");
 
@@ -180,8 +181,7 @@ public class TestFBSADataSource extends TestXABase {
             System.out.println(String.format("Engine version: %s", rs.getString(1)));
             rs.close();
             ps.close();
-        }
-        finally {
+        } finally {
             dataSource.close();
         }
     }
