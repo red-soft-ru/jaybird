80797661b3f8ea3d531f5c82477867ad14061bfe
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2021-04-21 10:25:43 +0300

    Cosmetic

--- src/test/org/firebirdsql/jca/TestFBSADataSource.java
+++ src/test/org/firebirdsql/jca/TestFBSADataSource.java
@@ -7,166 +7,166 @@ import org.junit.Test;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 
 import static org.firebirdsql.common.DdlHelper.executeCreateTable;
 import static org.firebirdsql.common.FBTestProperties.DB_DATASOURCE_URL;
 import static org.firebirdsql.common.FBTestProperties.getConnectionViaDriverManager;
 import static org.junit.Assert.assertEquals;
 
-public class TestFBSADataSource extends TestXABase
-{
-	@Test
-	public void testFBSADataSource() throws Exception {
+public class TestFBSADataSource extends TestXABase {
+    @Test
+    public void testFBSADataSource() throws Exception {
+        FBSADataSource dataSource = new FBSADataSource();
+        // Set the standard properties
+        dataSource.setDatabase(DB_DATASOURCE_URL);
+        dataSource.setDescription("An example database of employees");
+        dataSource.setUserName("sysdba");
+        dataSource.setPassword("masterkey");
+        dataSource.setEncoding("WIN1251");
+        try {
+            dataSource.setLoginTimeout(10);
+            Connection c1 = dataSource.getConnection();
+            Connection c2 = dataSource.getConnection();
+            c1.setAutoCommit(false);
+            c2.setAutoCommit(false);
+            Statement stmt = c1.createStatement();
+            Statement stmt2 = c2.createStatement();
+            ResultSet rs2 = stmt2.executeQuery("SELECT MON$ATTACHMENT_ID,MON$SERVER_PID FROM MON$ATTACHMENTS");
+            ResultSet rs = stmt.executeQuery("SELECT MON$TRANSACTION_ID,MON$ATTACHMENT_ID FROM MON$TRANSACTIONS");
 
-		org.firebirdsql.jca.FBSADataSource dataSource =
-				new org.firebirdsql.jca.FBSADataSource();
+            while (rs.next()) {
+                System.out.println(String.format("MON$TRANSACTION_ID = %s, MON$ATTACHMENT_ID = %s",
+                        rs.getString(1), rs.getString(2)));
+            }
 
-		// Set the standard properties
-		dataSource.setDatabase (DB_DATASOURCE_URL);
-		dataSource.setDescription ("An example database of employees");
-		dataSource.setUserName("sysdba");
-		dataSource.setPassword("masterkey");
-		dataSource.setEncoding("WIN1251");
-		try {
-			dataSource.setLoginTimeout (10);
-			java.sql.Connection c1 = dataSource.getConnection ();
-			java.sql.Connection c2 = dataSource.getConnection ();
-			c1.setAutoCommit(false);
-			c2.setAutoCommit(false);
-			java.sql.Statement stmt = c1.createStatement();
-			java.sql.Statement stmt2 = c2.createStatement();
-			java.sql.ResultSet rs2 = stmt2.executeQuery("SELECT MON$ATTACHMENT_ID,MON$SERVER_PID FROM MON$ATTACHMENTS");
-//	        java.sql.ResultSet rs = stmt.executeQuery("SELECT * FROM MON$ATTACHMENTS");
-			java.sql.ResultSet rs = stmt.executeQuery("SELECT MON$TRANSACTION_ID,MON$ATTACHMENT_ID FROM MON$TRANSACTIONS");
-			while(rs.next())
-				System.out.println("MON$TRANSACTION_ID = " + rs.getString(1) + ", MON$ATTACHMENT_ID = " + rs.getString(2));
+            while (rs2.next()) {
+                System.out.println(String.format("MON$ATTACHMENT_ID = %s, MON$SERVER_PID = %s",
+                        rs2.getString(1), rs2.getString(2)));
+            }
 
-			while(rs2.next())
-				System.out.println("MON$ATTACHMENT_ID = " + rs2.getString(1) + ", MON$SERVER_PID = " + rs2.getString(2));
-			stmt.close();
+            stmt.close();
 
-			// At this point, there is no implicit driver instance
-			// registered with the driver manager!
-			System.out.println ("got connection");
-			c1.close ();
-			c2.close ();
-			java.sql.Connection c3 = dataSource.getConnection ();
-			java.sql.PreparedStatement stmt3 = c3.prepareStatement("SELECT MON$ATTACHMENT_ID,MON$SERVER_PID FROM MON$ATTACHMENTS");
-	        rs = stmt3.executeQuery();
-	        while (rs.next()){
-	        	System.out.println(rs.getInt(1));
-			}
-			rs.close();
-	        c3.close();
-		}
-		finally {
-			dataSource.close();
-		}
-	}
+            // At this point, there is no implicit driver instance
+            // registered with the driver manager!
+            System.out.println("Connection established...");
+            c1.close();
+            c2.close();
+            Connection c3 = dataSource.getConnection();
+            PreparedStatement stmt3 = c3.prepareStatement("SELECT MON$ATTACHMENT_ID,MON$SERVER_PID FROM MON$ATTACHMENTS");
+            rs = stmt3.executeQuery();
 
-	@Test
-	public void testCreationConnectionAfterReadOnly() throws Exception {
-		final String CREATE_TABLE =
-				"CREATE TABLE test (" +
-						"  col1 INTEGER" +
-						")";
+            while (rs.next()) {
+                System.out.println(rs.getInt(1));
+            }
 
-		final String INSERT_DATA = "INSERT INTO test(col1) VALUES(?)";
+            rs.close();
+            c3.close();
+        } finally {
+            dataSource.close();
+        }
+    }
 
-		Connection connection = getConnectionViaDriverManager();
-		executeCreateTable(connection, CREATE_TABLE);
+    @Test
+    public void testCreationConnectionAfterReadOnly() throws Exception {
+        final String CREATE_TABLE =
+                "CREATE TABLE test (" +
+                        "  col1 INTEGER" +
+                        ")";
 
-		connection.setAutoCommit(false);
+        final String INSERT_DATA = "INSERT INTO test(col1) VALUES(?)";
 
-		PreparedStatement ps = connection.prepareStatement(INSERT_DATA);
-		ps.setInt(1, 1);
-		ps.execute();
-		ps.close();
+        Connection connection = getConnectionViaDriverManager();
+        executeCreateTable(connection, CREATE_TABLE);
 
-		connection.commit();
-		connection.close();
+        connection.setAutoCommit(false);
 
-		org.firebirdsql.jca.FBSADataSource dataSource =
-				new org.firebirdsql.jca.FBSADataSource();
+        PreparedStatement ps = connection.prepareStatement(INSERT_DATA);
+        ps.setInt(1, 1);
+        ps.execute();
+        ps.close();
 
-		// Set the standard properties
-		dataSource.setDatabase (DB_DATASOURCE_URL);
-		dataSource.setDescription ("An example database of employees");
-		dataSource.setUserName("sysdba");
-		dataSource.setPassword("masterkey");
-		dataSource.setEncoding("WIN1251");
-		try {
-			dataSource.setLoginTimeout (10);
-			FBConnection c1 = (FBConnection) dataSource.getConnection ();
+        connection.commit();
+        connection.close();
 
+        FBSADataSource dataSource = new FBSADataSource();
+        // Set the standard properties
+        dataSource.setDatabase(DB_DATASOURCE_URL);
+        dataSource.setDescription("An example database of employees");
+        dataSource.setUserName("sysdba");
+        dataSource.setPassword("masterkey");
+        dataSource.setEncoding("WIN1251");
+        try {
+            dataSource.setLoginTimeout(10);
+            FBConnection c1 = (FBConnection) dataSource.getConnection();
 
-			TransactionParameterBuffer tpb = c1.createTransactionParameterBuffer();
-			tpb.addArgument(TransactionParameterBuffer.READ_COMMITTED);
-			tpb.addArgument(TransactionParameterBuffer.REC_VERSION);
-			tpb.addArgument(TransactionParameterBuffer.READ);
-			tpb.addArgument(TransactionParameterBuffer.NOWAIT);
+            TransactionParameterBuffer tpb = c1.createTransactionParameterBuffer();
+            tpb.addArgument(TransactionParameterBuffer.READ_COMMITTED);
+            tpb.addArgument(TransactionParameterBuffer.REC_VERSION);
+            tpb.addArgument(TransactionParameterBuffer.READ);
+            tpb.addArgument(TransactionParameterBuffer.NOWAIT);
 
-			c1.setTransactionParameters(tpb);
+            c1.setTransactionParameters(tpb);
 
-			FBConnection c2 = (FBConnection) dataSource.getConnection ();
+            FBConnection c2 = (FBConnection) dataSource.getConnection();
 
-			c1.setAutoCommit(false);
-			c2.setAutoCommit(false);
-			java.sql.Statement stmt = c1.createStatement();
-			java.sql.Statement stmt2 = c2.createStatement();
-			java.sql.ResultSet rs = stmt.executeQuery("SELECT MON$TRANSACTION_ID,MON$ATTACHMENT_ID FROM MON$TRANSACTIONS");
-			int update = stmt2.executeUpdate("UPDATE test SET col1 = 2");
+            c1.setAutoCommit(false);
+            c2.setAutoCommit(false);
+            Statement stmt = c1.createStatement();
+            Statement stmt2 = c2.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT MON$TRANSACTION_ID,MON$ATTACHMENT_ID FROM MON$TRANSACTIONS");
+            int update = stmt2.executeUpdate("UPDATE test SET col1 = 2");
 
-			assertEquals(1, update);
+            assertEquals(1, update);
 
-			while(rs.next())
-				System.out.println("MON$TRANSACTION_ID = " + rs.getString(1) + ", MON$ATTACHMENT_ID = " + rs.getString(2));
+            while (rs.next()) {
+                System.out.println(String.format("MON$TRANSACTION_ID = %s, MON$ATTACHMENT_ID = %s",
+                        rs.getString(1), rs.getString(2)));
+            }
 
-			stmt.close();
-			stmt2.close();
+            stmt.close();
+            stmt2.close();
 
-			// At this point, there is no implicit driver instance
-			// registered with the driver manager!
-			System.out.println ("got connection");
-			c1.close ();
-			c2.close ();
-			java.sql.Connection c3 = dataSource.getConnection ();
-			java.sql.PreparedStatement stmt3 = c3.prepareStatement("SELECT MON$ATTACHMENT_ID,MON$SERVER_PID FROM MON$ATTACHMENTS");
-			rs = stmt3.executeQuery();
-			while (rs.next()){
-				System.out.println(rs.getInt(1));
-			}
-			rs.close();
-			c3.close();
-		}
-		finally {
-			dataSource.close();
-		}
-	}
+            // At this point, there is no implicit driver instance
+            // registered with the driver manager!
+            System.out.println("Connection established...");
+            c1.close();
+            c2.close();
+            Connection c3 = dataSource.getConnection();
+            PreparedStatement stmt3 =
+                    c3.prepareStatement("SELECT MON$ATTACHMENT_ID, MON$SERVER_PID FROM MON$ATTACHMENTS");
+            rs = stmt3.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getInt(1));
+            }
+            rs.close();
+            c3.close();
+        } finally {
+            dataSource.close();
+        }
+    }
 
-	@Test
-	public void testEncryptedPassword() throws Exception {
+    @Test
+    public void testEncryptedPassword() throws Exception {
+        FBSADataSource dataSource = new FBSADataSource();
+        // Set the standard properties
+        dataSource.setDatabase(DB_DATASOURCE_URL);
+        dataSource.setDescription("An example database of employees");
+        dataSource.setUserName("sysdba");
+        dataSource.setEncoding("WIN1251");
 
-		FBSADataSource dataSource =	new FBSADataSource();
-		// Set the standard properties
-		dataSource.setDatabase (DB_DATASOURCE_URL);
-		dataSource.setDescription ("An example database of employees");
-		dataSource.setUserName("sysdba");
-		dataSource.setEncoding("WIN1251");
-
-		// Legacy plugin use it
-		dataSource.setNonStandardProperty("isc_dpb_password_enc", "QP3LMZ/MJh.");
-		try (Connection connection = dataSource.getConnection()) {
-			PreparedStatement ps = connection.prepareStatement(
-					"select rdb$get_context('SYSTEM', 'ENGINE_VERSION') from rdb$database");
-			ResultSet rs = ps.executeQuery();
-			rs.next();
-			System.out.println(String.format("Engine version: %s", rs.getString(1)));
-			rs.close();
-			ps.close();
-		}
-		finally {
-			dataSource.close();
-		}
-	}
+        // Legacy plugin use it
+        dataSource.setNonStandardProperty("isc_dpb_password_enc", "QP3LMZ/MJh.");
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement ps = connection.prepareStatement(
+                    "select rdb$get_context('SYSTEM', 'ENGINE_VERSION') from rdb$database");
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            System.out.println(String.format("Engine version: %s", rs.getString(1)));
+            rs.close();
+            ps.close();
+        } finally {
+            dataSource.close();
+        }
+    }
 }
