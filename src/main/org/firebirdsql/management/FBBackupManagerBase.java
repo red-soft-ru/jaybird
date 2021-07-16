ae5b0b1cb8827ef1414dd91125ac6e611f60153a
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2021-07-16 13:05:24 +0300

    Remove unnecessary connection string parsing. See RS-65853
    Only path to a database is passed to the method, without specifying a server and port.

--- src/main/org/firebirdsql/management/FBBackupManagerBase.java
+++ src/main/org/firebirdsql/management/FBBackupManagerBase.java
@@ -122,19 +122,8 @@ public abstract class FBBackupManagerBase extends FBServiceManager implements Ba
     }
 
     public void setDatabase(String database) {
-        try {
-            final DbAttachInfo dbAttachInfo = DbAttachInfo.parseConnectString(database);
-            if (!dbAttachInfo.isDefaultServer())
-                super.setServer(dbAttachInfo.getServer());
-            if (!dbAttachInfo.isDefaultPort())
-                super.setPort(dbAttachInfo.getPort());
-            super.setDatabase(dbAttachInfo.getFileName());
-            addRestorePath(dbAttachInfo.getFileName(), -1);
-        } catch (SQLException e) {
-            super.setDatabase(database);
-            addRestorePath(database, -1);
-        }
-
+        super.setDatabase(database);
+        addRestorePath(database, -1);
         noLimitRestore = true;
     }
 
