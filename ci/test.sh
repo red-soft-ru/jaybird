5d69a3cbc302895c6d19fab1d76e8d77732667e9
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2021-11-29 15:55:37 +0300

    Do not run server to test embedded protocol

--- ci/test.sh
+++ ci/test.sh
@@ -231,33 +231,34 @@ elif [[ "$RDB_MAJOR_VERSION" == "3" ]]; then
   "${INSTALLDIR}"/bin/isql -user SYSDBA -password masterkey "${INSTALLDIR}"/security3.fdb -i "${SOURCES}"/ci/user3.sql
 fi
 
-# Delete symlink for fbclient to test fbclient.jar
-if [[ "$GDS_TYPE" != "EMBEDDED" && "$GDS_TYPE" != "FBOOEMBEDDED" ]]; then
-  rm -f /usr/lib64/libfbclient.so
-fi
-
-echo "Start RDB..."
-
-"$INSTALLDIR"/bin/rdbguard -daemon -forever
-
-(nc -h 2>&1|grep -q 'Zero-I/O mode') && NC="nc -z" || NC="nc --send-only"
-
-echo "Waiting until port 3050 opened..."
-try=10
-while ! $NC localhost 3050 </dev/null; do
-    sleep 5
-    try=$((try-1))
-    if [ $try = 0 ]; then
-        die "Unable to connect to RDB..."
-    fi
-done
-
-echo rdb_server | kinit rdb_server/localhost
-klist
-
 if [[ "$GDS_TYPE" == "" ]]; then
   GDS_TYPE="PURE_JAVA"
 fi
+
+# Delete symlink for fbclient to test fbclient.jar
+if [[ "$GDS_TYPE" != "EMBEDDED" && "$GDS_TYPE" != "FBOOEMBEDDED" ]]; then
+  rm -f /usr/lib64/libfbclient.so
+
+  echo "Start RDB..."
+
+  "$INSTALLDIR"/bin/rdbguard -daemon -forever
+  
+  (nc -h 2>&1|grep -q 'Zero-I/O mode') && NC="nc -z" || NC="nc --send-only"
+
+  echo "Waiting until port 3050 opened..."
+  try=10
+  while ! $NC localhost 3050 </dev/null; do
+      sleep 5
+      try=$((try-1))
+      if [ $try = 0 ]; then
+          die "Unable to connect to RDB..."
+      fi
+  done
+
+fi
+
+echo rdb_server | kinit rdb_server/localhost
+klist
 
 mvn $MAVEN_CLI_OPTS -f "${CI_PROJECT_DIR}"/pom.xml test -Pdeploy-internal -DreportNamePrefix=$REPORT_PREFIX -DreleaseHubBuildVersion=$VERSION  -DfailIfNoTests=false -Dtest.db.dir=$TEST_DIR -Dtest.java7.skip=$SKIP_JAVA7_TEST -Dtest.java8.jvm=$TEST_JAVA8_JVM -Dtest.java7.jvm=$TEST_JAVA7_JVM -Dtest=$TEST_LIST -Dtest.gds_type=$GDS_TYPE
 kdestroy
