369bde120ae320b7c7877cb21f0f6a16fa87f4e0
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2021-11-30 11:21:45 +0300

    Set length after type to make the parameter finished
    The metadata builder will check the parameter is finished after setting the length, but if the type is not set before then the parameter will be considered not finish

--- src/nativeoo/org/firebirdsql/nativeoo/gds/ng/IStatementImpl.java
+++ src/nativeoo/org/firebirdsql/nativeoo/gds/ng/IStatementImpl.java
@@ -217,17 +217,15 @@ public class IStatementImpl extends AbstractFbStatement {
                 // clear status
                 getStatus();
                 int length = inMeta.getLength(status, idx);
+                processStatus();
                 offset += length;
                 inMessage.position(nullOffset);
                 inMessage.put(nullShort);
                 offset += nullShort.length;
                 inMessage.position(offset);
                 // Although we pass a null value, length and type must still be specified
-                if (length == 0) {
-                    length = fieldDescriptor.getLength() != 0 ? fieldDescriptor.getLength() : Integer.MIN_VALUE;
-                }
-                metadataBuilder.setLength(status, idx, length);
                 metadataBuilder.setType(status, idx, inMeta.getType(status, idx) | 1);
+                metadataBuilder.setLength(status, idx, inMeta.getLength(status, idx));
                 metadataBuilder.setCharSet(status, idx, inMeta.getCharSet(status, idx));
             } else {
 
@@ -239,10 +237,8 @@ public class IStatementImpl extends AbstractFbStatement {
                 inMessage.position(offset);
                 if (fieldDescriptor.isVarying()) {
                     int length = Math.min(fieldDescriptor.getLength(), fieldData.length);
-                    if (length == 0)
-                        length = fieldDescriptor.getLength() != 0 ? fieldDescriptor.getLength() : Integer.MIN_VALUE;
-                    metadataBuilder.setLength(status, idx, length);
                     metadataBuilder.setType(status, idx, ISCConstants.SQL_VARYING + 1);
+                    metadataBuilder.setLength(status, idx, length);
                     metadataBuilder.setCharSet(status, idx, inMeta.getCharSet(status, idx));
                     byte[] encodeShort = fieldDescriptor.getDatatypeCoder().encodeShort(fieldData.length);
                     inMessage.put(encodeShort);
@@ -250,39 +246,36 @@ public class IStatementImpl extends AbstractFbStatement {
                     inMessage.position(offset);
 
                 } else if (fieldDescriptor.isFbType(ISCConstants.SQL_TEXT)) {
-                    metadataBuilder.setLength(status, idx, Math.min(fieldDescriptor.getLength(), fieldData.length));
                     metadataBuilder.setType(status, idx, ISCConstants.SQL_TEXT + 1);
+                    metadataBuilder.setLength(status, idx, Math.min(fieldDescriptor.getLength(), fieldData.length));
                     metadataBuilder.setCharSet(status, idx, inMeta.getCharSet(status, idx));
                 } else if (fieldDescriptor.isFbType(ISCConstants.SQL_BLOB)) {
                     if (offset != 0)
                         offset += offset % alignment;
                     inMessage.position(offset);
-                    metadataBuilder.setLength(status, idx, inMeta.getLength(status, idx));
                     metadataBuilder.setType(status, idx, ISCConstants.SQL_BLOB + 1);
+                    metadataBuilder.setLength(status, idx, inMeta.getLength(status, idx));
                     metadataBuilder.setCharSet(status, idx, inMeta.getCharSet(status, idx));
-
-
                 } else if (fieldDescriptor.isFbType(ISCConstants.SQL_TYPE_TIME)) {
                     if (offset != 0)
                         offset += offset % alignment;
                     inMessage.position(offset);
-                    metadataBuilder.setLength(status, idx, inMeta.getLength(status, idx));
                     metadataBuilder.setType(status, idx, ISCConstants.SQL_TYPE_TIME + 1);
+                    metadataBuilder.setLength(status, idx, inMeta.getLength(status, idx));
                     metadataBuilder.setCharSet(status, idx, inMeta.getCharSet(status, idx));
                 } else if (fieldDescriptor.isFbType(ISCConstants.SQL_TIMESTAMP)) {
                     if (offset != 0)
                         offset += offset % alignment;
                     inMessage.position(offset);
-                    metadataBuilder.setLength(status, idx, inMeta.getLength(status, idx));
                     metadataBuilder.setType(status, idx, ISCConstants.SQL_TIMESTAMP + 1);
+                    metadataBuilder.setLength(status, idx, inMeta.getLength(status, idx));
                     metadataBuilder.setCharSet(status, idx, inMeta.getCharSet(status, idx));
                 } else {
                     if (offset % alignment != 0)
                         offset += offset % alignment;
                     inMessage.position(offset);
-                    int length = inMeta.getLength(status, idx);
-                    metadataBuilder.setLength(status, idx, length != 0 ? length : Integer.MIN_VALUE);
                     metadataBuilder.setType(status, idx, fieldDescriptor.getType());
+                    metadataBuilder.setLength(status, idx, inMeta.getLength(status, idx));
                     metadataBuilder.setCharSet(status, idx, 0);
                 }
                 inMessage.put(fieldData);
