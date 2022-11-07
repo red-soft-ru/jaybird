a3aecc5610e94280417b03b4a8a6d983881f6f17
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2022-11-07 16:11:38 +0300

    Fix parameter buffer creation for service when connecting via native interfaces. See RS-90026

--- src/nativeoo/org/firebirdsql/nativeoo/gds/ng/IParameterConverterImpl.java
+++ src/nativeoo/org/firebirdsql/nativeoo/gds/ng/IParameterConverterImpl.java
@@ -8,6 +8,8 @@ import org.firebirdsql.gds.ng.WireCrypt;
 import org.firebirdsql.jdbc.FirebirdConnectionProperties;
 
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class IParameterConverterImpl extends AbstractParameterConverter<NativeDatabaseConnection, IServiceConnectionImpl> {
 
@@ -22,11 +24,35 @@ public class IParameterConverterImpl extends AbstractParameterConverter<NativeDa
         if (props.getPassword() != null) {
             pb.addArgument(tagMapping.getPasswordTag(), props.getPassword());
         }
+        if (props.getEffectiveLogin() != null) {
+            pb.addArgument(tagMapping.getEffectiveLoginTag(), props.getEffectiveLogin());
+        }
+
+        Map<String, String> configMap = new HashMap<>();
 
         if (WireCrypt.fromString(props.getWireCrypt()) != WireCrypt.DEFAULT) {
-            // Need to do this differently when having to add multiple configs
-            String configString = "WireCrypt = " + props.getWireCrypt();
+            configMap.put("WireCrypt", props.getWireCrypt());
+        }
+
+        String authPlugins = props.getAuthPlugins();
+        if (authPlugins != null && !authPlugins.isEmpty()) {
+            configMap.put("AuthClient", authPlugins);
+        }
+
+        if (!configMap.isEmpty()) {
+            String configString = buildConfigString(configMap);
             pb.addArgument(tagMapping.getConfigTag(), configString);
         }
+    }
+
+    private String buildConfigString(Map<String, String> configMap) {
+        StringBuilder builder = new StringBuilder();
+        for (Map.Entry<String, String> configEntry : configMap.entrySet()) {
+            builder.append(configEntry.getKey())
+                    .append('=')
+                    .append(configEntry.getValue())
+                    .append('\n');
+        }
+        return builder.toString();
     }
 }
