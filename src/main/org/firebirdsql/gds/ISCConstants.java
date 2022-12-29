bb3a2436238345cced1b2e5589510bc25ef39583
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2022-12-29 15:48:06 +0300

    Shift the connection parameters for internal use. See RS-94092

--- src/main/org/firebirdsql/gds/ISCConstants.java
+++ src/main/org/firebirdsql/gds/ISCConstants.java
@@ -283,9 +283,9 @@ public interface ISCConstants {
     int isc_dpb_parallel_workers        = 167;
     int isc_dpb_worker_attach           = 168;
     int isc_dpb_certificate_base64      = 169;
-    int isc_dpb_exclude_crypto_plugins  = 170;
-    int isc_dpb_provider_id             = 171;
-    int isc_dpb_not_encrypt_password    = 172;
+    int isc_dpb_exclude_crypto_plugins  = 1001;
+    int isc_dpb_provider_id             = 1002;
+    int isc_dpb_not_encrypt_password    = 1003;
 
     // Firebird 4 constants
     @Deprecated
